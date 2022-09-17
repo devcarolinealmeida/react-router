9a42db719fb14d7116fba7110cc95b071d8ae1cd
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-17 09:39:59 +0200

    build: add Header and nav Link

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Pagina404 from './pages/Page404';
+import Header from './components/Header';
 
 
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route exact path={'/'} element={<Home/>}/>
         <Route path={'/sobre'} element={<Sobre />}/>
---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import imageDoguito from '../assets/img/doguito.svg'
+import '../assets/css/componentes/cabecalho.css'
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return ( 
+        <header className='cabecalho container'>
+            <div className='menu-hamburguer'>
+                <span className='menu-hamburguer_icone'></span>
+            </div>
+            <div className='cabecalho-container'>
+                <Link to='/' className='flex flex--centro'>
+                    <img className='cabecalho__logo' src={imageDoguito} alt="Logo Doguito" />
+                    <h1 className='cabecalho__titulo'>PetShop</h1>
+                </Link>
+            </div>
+            <nav className='menu-cabecalho'>
+                <ul className='menu-itens'>
+                    <li><Link to='#' className='menu-item menu-item--entrar'>Entrar</Link></li>
+                    <li><Link to='#' className='menu-item'>Produtos</Link></li>
+                    <li><Link href='/' className='menu-item'>Blog</Link></li>
+                    <li><Link to='/sobre' className='menu-item'>Sobre</Link></li>
+                </ul>
+            </nav>
+            <div className='menu-cabecalho-background'></div>
+        </header>
+     );
+}
+ 
+export default Header;
