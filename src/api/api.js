3edc675953806d6a2e9817df08edffe3ba78935d
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-17 12:38:09 +0200

    add post list and conect with intern api db.json

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,11 @@
-import axios from "axios";
+import axios from 'axios'
+
 
 export const api = axios.create({
-    baseURL: 'http://localhost:5000'
+  baseURL: 'http://localhost:5000'
 })
 
-export const busca = async() => {
-    const resposta = await api.get(url)
-    setDado(resposta.data)
+export const busca = async(url, setDado) => { 
+  const reposta = await api.get(url)
+  setDado(reposta.data)
 }
